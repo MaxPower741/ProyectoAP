71577a0896a0a13d58b218ced3cf89e197f37c86
MaxPower741 <max_medina86@hotmail.com>
2022-07-27 23:48:16 -0300

    Fin Front-End

--- ProyectoAP/src/app/app.module.ts
+++ ProyectoAP/src/app/app.module.ts
@@ -5,30 +5,45 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HabilidadesComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 12,
+      innerStrokeWidth: 12,
+      space: -12,
+      outerStrokeGradient: true,
+      outerStrokeColor: "#10E010",
+      outerStrokeGradientStopColor: "#F01010",
+      innerStrokeColor: "#D0D0D0",
+      animationDuration: 600,
+      titleFontSize: "30",
+      subtitleFontSize: "20",
+      showUnits: false,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
